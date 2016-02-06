06678a883fca18e912ad7b9a63f30ab7763eda89
hudginst <hudginst@mymail.vcu.edu>
2016-02-05 23:42:06 -0500

    Update twitter_search.py

--- twitter_search.py
+++ twitter_search.py
@@ -3,7 +3,7 @@ from pattern.web import Twitter
 t = Twitter()
 i = None
 for j in range(3):
-    for tweet in t.search('college', start=i, count=30):
+    for tweet in t.search('signing day', start=i, count=30):
         print tweet.id
         print tweet.name
         print tweet.text
